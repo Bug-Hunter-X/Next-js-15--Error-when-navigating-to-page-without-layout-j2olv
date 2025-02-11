09d25847df0302cc157798c9435fd42c80dcd63a
BugHunter-X <askzenai@gmail.com>
2025-02-11 23:24:07 +0100

    Add index.js file

---
+++ index.js
@@ -0,0 +1,38 @@
+```javascript
+// app/layout.js
+import './globals.css';
+
+function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
+
+export default RootLayout;
+```
+```javascript
+// app/page.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+      <Link href='/about'>
+        <a>Go to About Page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// app/about/page.js
+
+export default function About() {
+  return (
+    <h1>About Page</h1>
+  );
+}
+```
